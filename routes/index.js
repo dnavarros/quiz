8edf3897fecb6e77ce1bc1ff1c026e0b21fe079b
David Navarro <dnavaserra@gmail.com>
2015-07-07 18:16:09 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var quizController = require ('../controllers/quiz_controller.js'); //Importamos el controlador de quiz
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+//GET Pregunta y respuesta, haciendo lo que dice el controlador que hemos importado arriba
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
