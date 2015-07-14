234272075fbaa85fb463cfeaf3580da4ceedb2cc
David Navarro <dnavaserra@gmail.com>
2015-07-14 19:22:50 +0200

    Autoload de la DB

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Autoload de comandos con :quizId
+//SOLO CARGA SI EN LA RUTA HAY UN PARÁMETRO 'quizID'
+/*quizController.load() se instala para que se ejecute antes que lo necesiten las rutas show y
+answer y solo en el caso de que path contenga :quizId, referenciando un recurso en la tabla
+Quiz de la base de datos que deba ser procesado por el controlador.
+Se instala con el método param() de express (http://expressjs.com/4x/api.html#router.param),
+para que router.param(‘quizId’, quizController.load) solo invoque quizController.load si
+existe el parámetro :quizId está en algún lugar de la cabecera HTTP (en query, body o param).*/
+router.param('quizId', quizController.load); // autoload :quizID
+
 //Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
