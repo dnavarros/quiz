9512efcb8d887e87d2c4c79f8af951181ece4133
David Navarro <dnavaserra@gmail.com>
2015-07-22 18:06:45 +0200

    Buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,13 +13,24 @@ exports.load = function(req, res, next, quizId){
   ).catch(function(error){next(error);});
 };
 
-// GET /quizes
+// GET /quizes y Buscador de preguntas "search"
 exports.index = function(req, res) {
-    models.Quiz.findAll().then(function(quizes) {
-      res.render('quizes/index.ejs', { quizes: quizes});
-     }
-    ).catch(function (error) {next(error);});
- };
+var search = "%";
+
+if(req.query.search != undefined)
+{
+search = "%" + req.query.search.trim() + "%";
+search = search.replace(/\s+/g,"%");
+}
+
+models.Quiz.findAll({where:["upper(pregunta) like ?", search.toUpperCase()], order: 'pregunta ASC'}).
+
+then(
+function(quizes) {
+res.render('quizes/index', { quizes: quizes, errors: []});
+}
+).catch(function(error) { next(error);})
+};
 
 // GET /quizes/:id
 exports.show = function(req, res) {
